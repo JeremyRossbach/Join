1095451dfb234857e2c42d5d49a2412fd9df9f1d
Jeremy Roßbach <jeremy1899@web.de>
2024-04-12 19:30:27 +0200

    added data to data.js/board progress

---
+++ data.js
@@ -0,0 +1,82 @@
+let contactData = [
+    {
+        'name': 'Anton Mayer',
+        'e-mail': 'antom@gmail.com',
+        'phoneNumber': '+49 1111 111 11 1'
+    },
+    {
+        'name': 'Anja Schulz',
+        'e-mail': 'schulz@hotmail.com',
+        'phoneNumber': '+49 2222 222 22 2'
+    },
+    {
+        'name': 'Benedikt Ziegler',
+        'e-mail': 'benedikt@gmail.com',
+        'phoneNumber': '+49 3333 333 33 3'
+    },
+    {
+        'name': 'David Eisenberg',
+        'e-mail': 'davidberg@gmail.com',
+        'phoneNumber': '+49 4444 444 44 4'
+    },
+    {
+        'name': 'Eva Fischer',
+        'e-mail': 'eva@gmail.com',
+        'phoneNumber': '+49 5555 555 55 5'
+    },
+    {
+        'name': 'Emmanuel Mauer',
+        'e-mail': 'emmanuelma@gmail.com',
+        'phoneNumber': '+49 6666 666 66 6'
+    },
+    {
+        'name': 'Marcel Bauer',
+        'e-mail': 'bauer@gmail.com',
+        'phoneNumber': '+49 7777 777 77 7'
+    },
+    {
+        'name': 'Tatjana Wolf',
+        'e-mail': 'wolf@gmail.com',
+        'phoneNumber': '+49 8888 888 88 8'
+    }
+];
+
+
+let tasks = [
+    {
+        'title': 'Contact Form & Imprint',
+        'description': 'Create a contact form and imprint page...',
+        'assignedTo': ['Anja Schulz', 'David Eisenberg', 'Eva Fischer'],
+        'dueDate': '01/01/2024',
+        'prio': 'Urgent',
+        'category': 'User Story',
+        'subtask': ['', '']
+    },
+    {
+        'title': 'HTML Base Tamplate Creation',
+        'description': 'Create reusable HTML base tamplates...',
+        'assignedTo': ['David Eisenberg', 'Benedikt Ziegler', 'Anja Schulz'],
+        'dueDate': '01/01/2024',
+        'prio': 'Low',
+        'category': 'Technical Task',
+        'subtask': ['']
+    },
+    {
+        'title': 'Daily Kochwelt Recipe',
+        'description': 'Implement daily recipe and portion caltulator...',
+        'assignedTo': ['Eva Fischer', 'Anja Schulz', 'Tatjana Wolf'],
+        'dueDate': '01/01/2024',
+        'prio': 'Medium',
+        'category': 'User Story',
+        'subtask': ['']
+    },
+    {
+        'title': 'CSS Architecture Planning',
+        'description': 'Define CSS naming conventions nad structure.',
+        'assignedTo': ['Sofia Müller', 'Benedikt Ziegler'],
+        'dueDate': '02/09/2023',
+        'prio': 'Urgent',
+        'category': 'Technical Task',
+        'subtask': ['Establish CSS Methodology', 'Setup Base Styles']
+    }
+];
