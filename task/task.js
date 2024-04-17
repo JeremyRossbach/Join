686900b15331c02301fd3e210b30102cd540296f
marcelstiefs <marcel.stiefs@web.de>
2024-04-17 15:31:09 +0200

    addtask4

--- task/task.js
+++ task/task.js
@@ -1,18 +1,21 @@
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
+   
 }
 
-function filterFunction() {
-    const input = document.getElementById("myInput");
-    const filter = input.value.toUpperCase();
-    const div = document.getElementById("myDropdown");
-    const a = div.getElementsByTagName("a");
-    for (let i = 0; i < a.length; i++) {
-        txtValue = a[i].textContent || a[i].innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            a[i].style.display = "";
-        } else {
-            a[i].style.display = "none";
-        }
+
+function showaAvailableContacts() {
+    let availableContacts = document.getElementById("availableContacts");
+    let contacts = contactData
+    availableContacts.innerHTML='';
+    for (let i = 0; i < contacts.length; i++) {
+        const element = contacts[i];
+        availableContacts.innerHTML+= /*html*/`<div class="contactBTN">
+            
+        
+            <div id="contacts${i}">${element['name']} </div>
+            <img src="img/selectionbox unclicked.svg" alt="">
+        </div>
+        `
     }
 }
