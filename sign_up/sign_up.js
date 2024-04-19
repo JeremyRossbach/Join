c227437756ac2a77c000bfceed91e6e515034517
FabioSpoto2023 <FabioSpoto2023@gmail.com>
2024-04-19 09:38:23 +0200

    added dialog message animation to sign up

--- sign_up/sign_up.js
+++ sign_up/sign_up.js
@@ -1,11 +1,15 @@
-function checkPasswordMatch() {
-    let form = document.forms['sign-up'];
-
+function checkPasswordMatch(form) {
     if (form['password'].value != form['confirm-password'].value) {
         document.querySelector('div[class="validation-password"]').style.display = '';
         form['confirm-password'].classList.add('input-danger');
         return false;
     }
+    
+    document.getElementById('dialog-message').style.display = '';
 
-    return true;
+    setTimeout(function() {
+        form.submit();
+    }, 2000);
+
+    return false;
 }
