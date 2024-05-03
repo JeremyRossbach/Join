ed841f318311db82c26ac0e37e65d9e1d8391502
marcelstiefs <marcel.stiefs@web.de>
2024-05-03 15:09:55 +0200

    addtask10.

--- task/task.js
+++ task/task.js
@@ -4,24 +4,28 @@ let taskcontacts = [];
 let currentContact;
 let prio;
 let subtasks = ['test',];
+let selectedContackts = [];
 
 
-
-function initAddTasks(){
+function initAddTasks() {
     loadSubtasks();
-   
-}
+    renderSubtask();
+    showaAvailableContacts();
 
+}
 
 
 function dropDownContacts() {
-    document.getElementById("myDropdown").classList.toggle("show");
-    arrowChange()
+    document.getElementById("contacts_dropdown").classList.toggle("show");
+    arrowChange();
 }
+
+
 function dropDownCategory() {
-    document.getElementById("drop_down_category").classList.toggle("show");
+    document.getElementById('availableCategory').classList.toggle("show");
     arrowChangeCategory();
 }
+
 
 function arrowChange() {
     let arrow = document.getElementById("arrow");
@@ -63,6 +67,8 @@ function showaAvailableContacts() {
         `
     }
 }
+
+
 function chosenContacts(i, currentContact) {
     let chosenContact = contactData[i];
     let selectionBox = document.getElementById(`selectionBox${i}`)
@@ -71,17 +77,17 @@ function chosenContacts(i, currentContact) {
     let clicked_background = "background-color:rgb(42,54,71);";
     let selection_box_unclicked = "img/selectionbox unclicked.svg";
     let selection_box_clicked = "img/selectionbox clicked.svg";
-    if( selectionBox.getAttribute('src') == selection_box_unclicked){
-        selectionBox.setAttribute('src',selection_box_clicked)
-        ContactBTN.setAttribute('style',clicked_background)
-       
-    }else{
+    if (selectionBox.getAttribute('src') == selection_box_unclicked) {
+        selectionBox.setAttribute('src', selection_box_clicked)
+        ContactBTN.setAttribute('style', clicked_background)
+
+    } else {
         selectionBox.src = selection_box_unclicked;
         ContactBTN.style = unclicked_background;
-       
+
     }
-    chosenContactsPush(i, selection_box_clicked )
-    
+    chosenContactsPush(i, selection_box_clicked)
+    console.log(taskcontacts);
 }
 
 function chosenContactsPush(i, selection_box_clicked) {
@@ -101,8 +107,9 @@ function chosenContactsPush(i, selection_box_clicked) {
     showchosenInitials(i)
     console.log(taskcontacts);
 }
-function showchosenInitials(i){
-    
+
+
+function showchosenInitials(i) {
     let chosenInitals = document.getElementById('chosenInitals');
     chosenInitals.innerHTML = ""
     for (let index = 0; index < taskcontacts.length; index++) {
@@ -117,26 +124,26 @@ function showchosenInitials(i){
 }
 
 
-function prioUrgent(){
+function prioUrgent() {
     let highicon = document.getElementById("highicon");
     let urgentbtn = document.getElementById('urgentbtn');
     let clickedhighicon = "img/Prio high icon clicked.svg"
     let unclickedhighicon = "img/Prio high icon.svg"
-    if (highicon.getAttribute('src') == unclickedhighicon){
+    if (highicon.getAttribute('src') == unclickedhighicon) {
         highicon.setAttribute('src', clickedhighicon)
         urgentbtn.style.background = 'rgb(255, 61, 0)';
-        urgentbtn.style.color ='#FFFFFF';
+        urgentbtn.style.color = '#FFFFFF';
         prioMedSetBack()
         prioLowSetBack()
     } else {
         highicon.src = unclickedhighicon
-        urgentbtn.style.background ='#FFFFFF';
+        urgentbtn.style.background = '#FFFFFF';
         urgentbtn.style.color = 'black';
     }
 }
 
 
-function prioMed(){
+function prioMed() {
     let medbtn = document.getElementById('midbtn');
     let medicon = document.getElementById('midicon');
     let unclickedmidicon = 'img/Prio medi icon.svg'
@@ -154,16 +161,16 @@ function prioMed(){
     }
 }
 
- 
+
 let lowbtn = document.getElementById('lowbtn');
 let lowicon = document.getElementById("lowicon");
- let unclickedlowcon = "img/Prio low icon.svg";
+let unclickedlowcon = "img/Prio low icon.svg";
 let clickedlowicon = "img/Prio low icon clicked.svg";
 
 
 function prioLow() {
     let lowicon = document.getElementById("lowicon");
-   let lowbtn = document.getElementById('lowbtn');
+    let lowbtn = document.getElementById('lowbtn');
 
     if (lowicon.getAttribute('src') == unclickedlowcon) {
         lowicon.setAttribute('src', clickedlowicon);
@@ -177,25 +184,31 @@ function prioLow() {
         lowbtn.style.color = 'black';
     }
 }
-function prioLowSetBack(){
+
+
+function prioLowSetBack() {
     let lowbtn = document.getElementById('lowbtn');
     let lowicon = document.getElementById("lowicon");
     let unclickedlowcon = "img/Prio low icon.svg";
-    
+
     lowicon.src = unclickedlowcon;
     lowbtn.style.background = '#FFFFFF';
     lowbtn.style.color = 'black';
 }
-function prioMedSetBack(){
+
+
+function prioMedSetBack() {
     let medbtn = document.getElementById('midbtn');
     let medicon = document.getElementById('midicon');
     let unclickedmidicon = 'img/Prio medi icon.svg'
-    
+
     medicon.src = unclickedmidicon
     medbtn.style.background = '#FFFFFF';
     medbtn.style.color = 'black';
 }
-function prioUrgentSetBack(){
+
+
+function prioUrgentSetBack() {
     let highicon = document.getElementById("highicon");
     let urgentbtn = document.getElementById('urgentbtn');
     let unclickedhighicon = "img/Prio high icon.svg"
@@ -206,90 +219,99 @@ function prioUrgentSetBack(){
 }
 
 
-
-
-function technicalTask(){
+function technicalTask() {
     let categorySpan = document.getElementById('categorySpan');
     categorySpan.innerHTML = "Technikal Task";
     categorys = "Technikal Task";
 }
- 
 
-function userStory(){
+
+function userStory() {
     let categorySpan = document.getElementById('categorySpan');
     categorySpan.innerHTML = "User Story";
     categorys = "User Story";
 }
 
 function renderSubtask() {
-
     let pushedSubtasks = document.getElementById("pushedSubtasks");
     pushedSubtasks.innerHTML = '';
     for (let j = 0; j < subtasks.length; j++) {
         let subtask = subtasks[j];
-
-        pushedSubtasks.innerHTML +=/*html*/`
-            <div id='editableSubtask${j}' onclick="subtasksEditAndDelete(${j},'${subtask}')">· ${subtask}</div>
-        `
+        pushedSubtasks.innerHTML += generateSubtaskHTML(j, subtask);
     }
 }
 
-
-function subtasksEditAndDelete(j, subtask) {
-    let editableSubtask = document.getElementById(`editableSubtask${j}`);
-
-    editableSubtask.innerHTML = /*html*/`
-        <div class='subtaskEdit'>
-            <input id='subtaskEdit' value='${subtask}'>
-            <img onclick='deleteSubtask(${j})' src="img/subtaskDelete.svg">
-            <div class='divider'></div>
-                   <img onclick='pushEditToArray(${j})' src="img/subtaskCheck.svg">
-        </div>
+function generateSubtaskHTML(index, subtask) {
+    return /*html*/`
+        <div class="subtask">
+            <div class='subtaskHover'>
+                <div class='editableSubtask' id='editableSubtask${index}'>
+                    <span id='valueSpan${index}'>· ${subtask}</span>
+                    <div id='subtaskHoverIcon${index}' class="subtaskHoverIcon">
+                        <img onclick='subtasksEditAndDelete(${index})' src="img/subtaskPencil.svg">
+                        <img onclick='deleteSubtask(${index})' src="img/subtaskDelete.svg">
+                    </div>
+                </div>
+            </div>
+            <div class='subtaskInputParent noDisplay' id='subtaskInputParent${index}'>
+                <input class='subtaskInput noDisplay' id='subtaskEditInput${index}' value='${subtask}'>
+                <img onclick='deleteSubtask(${index})' src="img/subtaskDelete.svg">
+                <div class='divider'></div>
+                <img onclick='pushEditToArray(${index},"${subtask}")' src="img/subtaskCheck.svg">
+            </div>
         </div>
     `;
-    subtaskEdit.focus()
-    
 }
 
-//function saveEditedSubtask(j) {
-  //  let editedSubtaskInput = document.getElementById(`subtaskEdit`);
-   // sub_tasks[j] = editedSubtaskInput.value;
-   
-//}
+
+function subtasksEditAndDelete(j) {
+    let editableSubtask = document.getElementById(`editableSubtask${j}`);
+    document.getElementById(`subtaskEditInput${j}`).classList.remove('noDisplay');
+    document.getElementById(`subtaskInputParent${j}`).classList.remove('noDisplay');
+    document.getElementById(`subtaskEditInput${j}`).focus();
+    document.getElementById(`subtaskHoverIcon${j}`).classList.add('noDisplay');
+    document.getElementById(`editableSubtask${j}`).classList.add('noDisplay');
+    document.getElementById(`valueSpan${j}`).classList.add('noDisplay');
+}
+
+
 
 function deleteSubtask(j) {
     subtasks.splice(j, 1);
     saveSubtusks();
-    
     renderSubtask();
 }
-function pushEditToArray(j){
-    let subtaskEdit = document.getElementById('subtaskEdit')
-    
+
+
+function pushEditToArray(j) {
+    let subtaskEdit = document.getElementById(`subtaskEditInput${j}`)
+
     subtasks.push(subtaskEdit.value)
     subtaskEdit.value = '';
-   deleteSubtask(j)
+    deleteSubtask(j)
     console.log(subtasks)
-    
+
     saveSubtusks();
     loadSubtasks();
     renderSubtask();
-} 
-function saveSubtusks(){
-    let subtaskasText = JSON.stringify(subtasks);
-    localStorage.setItem('subtasks',subtaskasText);
 }
-function loadSubtasks(){
+
+
+function saveSubtusks() {
+    let subtaskasText = JSON.stringify(subtasks);
+    localStorage.setItem('subtasks', subtaskasText);
+}
+
+
+function loadSubtasks() {
     let subtaskasText = localStorage.getItem(subtasks)
     sub_tasks = JSON.parse(subtaskasText);
-    
+
 }
 function pushSubtask() {
     let subtaskinput = document.getElementById("subtask_input");
-    
     subtasks.push(subtaskinput.value);
-  
     subtaskinput.value = '';
-   
+
     renderSubtask()
 }
