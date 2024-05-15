056c899d510cae4e9d1751b20d3585a34c70979a
marcelstiefs <marcel.stiefs@web.de>
2024-05-15 09:54:00 +0200

    addtask new js.

---
+++ task/createtask3.js
@@ -0,0 +1,104 @@
+let testtask = [];
+
+function createTask() {
+    let titleInput = document.getElementById('titleInput');
+    let descriptionInput = document.getElementById('descriptionInput');
+    let date = document.getElementById('date');
+    let dangerTexts = document.getElementsByClassName('danger-text');
+
+    let newTask = {
+        'title': titleInput.value,
+        'description': descriptionInput.value,
+        'assignedTo': taskcontacts,
+        'dueDate': date.value,
+        'prio': prio,
+        'category': categorys,
+        'subtask': subtasks,
+        'doneSubtask': subtasks.map(() => false),
+        'numberOfDoneSubtasks': 0,
+        'section': 'To do'
+    }
+
+    testtask.push(newTask);
+    clearForm();
+    console.log(testtask)
+    putData("tasks", tasks)
+        .then(response => console.log(response))
+        .catch(error => console.error(error));
+}
+
+function clearForm() {
+    document.getElementById('titleInput').value = '';
+    document.getElementById('descriptionInput').value = '';
+    document.getElementById('date').value = '';
+    document.getElementById('categorySpan').innerHTML = 'Select tasks category';
+    subtasks = [];
+    taskcontacts = [];
+    prio = '';
+    renderSubtask();
+    showaAvailableContacts();
+    showchosenInitials();
+    prioUrgentSetBack();
+    prioMedSetBack();
+    prioLowSetBack();
+    createBtnEnable();
+
+
+}
+
+inputDateColorChange();
+function inputDateColorChange() {
+    let inputDate = document.getElementById('date');
+    if (inputDate.value) {
+        inputDate.style.color = 'black';
+    } else {
+        inputDate.style.color = 'grey';
+    }
+}
+
+function createBtnEnable() {
+    let alarmBTN = document.getElementById("alarmBTN");
+    let createBTN = document.getElementById("createBTN");
+    let date = document.getElementById('date');
+    let titleInput = document.getElementById('titleInput');
+
+    if (titleInput.value && date.value && categorys !== null && categorys !== undefined) {
+        createBTN.classList.remove('noDisplay');
+        alarmBTN.classList.add('noDisplay');
+
+    } else {
+        createBTN.classList.add('noDisplay');
+        alarmBTN.classList.remove('noDisplay');
+    }
+}
+
+function alarmInput() {
+    let dropBtnCategory = document.getElementById("dropBtnCategory")
+    let titleInput = document.getElementById('titleInput');
+    let categoryDangerText = document.getElementById("categoryDangerText");
+    let date = document.getElementById('date');
+    let dangerTexts = document.getElementsByClassName('danger-text');
+    if (!titleInput.value) {
+        titleInput.classList.add('input-field-danger');
+        dangerTexts[0].style.display = '';
+    } else {
+        titleInput.classList.remove('input-field-danger');
+        dangerTexts[0].style.display = 'none';
+    }
+
+    if (!date.value) {
+        date.classList.add('input-field-danger');
+        dangerTexts[1].style.display = '';
+    } else {
+        date.classList.remove('input-field-danger');
+        dangerTexts[1].style.display = 'none';
+    }
+    if (categorys !== null && categorys !== undefined) {
+        dropBtnCategory.classList.remove('input-field-danger');
+        categoryDangerText.style.display = 'none';
+
+    } else {
+        dropBtnCategory.classList.add('input-field-danger');
+        categoryDangerText.style.display = '';
+    }
+}
