083fe43abac237cbb94b9fd7ca92bd29f87a384d
FabioSpoto2023 <FabioSpoto2023@gmail.com>
2024-05-20 12:03:10 +0200

    jsdoc

--- login.js
+++ login.js
@@ -1,21 +1,26 @@
-let inputsPassword = document.querySelectorAll('input[type="password"]');
+let inputPassword = document.querySelector('input[type="password"]');
 let validationText = document.querySelector('div[class="validation-password"]');
 let buttonsPasswordVisibility = document.querySelectorAll('div[class="password-visibility"]')
 
-function checkValidPassword() {
-    for (let i = 0; i < inputsPassword.length; i++) {
-        let inputPassword = inputsPassword[i];
 
-        if (inputPassword.value.length < 1) {
-            validationText.style.display = '';
-            inputPassword.classList.add('input-danger');
-            return false;
-        }
+/**
+ * Check if the user have left the password input empty or not
+ * @returns {boolean} Return true if password input have min. 1 value and return false if input are empty
+ */
+function checkValidPassword() {
+    if (inputPassword.value.length < 1) {
+        validationText.style.display = '';
+        inputPassword.classList.add('input-danger');
+        return false;
     }
 
     return true;
 }
 
+
+/**
+ * Show password in the password input
+ */
 function showButtonPasswordVisible() {
     for (let i = 0; i < buttonsPasswordVisibility.length; i++) {
         let buttonPasswordVisibility = buttonsPasswordVisibility[i];
@@ -23,6 +28,9 @@ function showButtonPasswordVisible() {
     }
 }
 
+/**
+ * Hide password in the password input
+ */
 function hideButtonPasswordVisible() {
     for (let i = 0; i < buttonsPasswordVisibility.length; i++) {
         let buttonPasswordVisibility = buttonsPasswordVisibility[i];
@@ -30,33 +38,42 @@ function hideButtonPasswordVisible() {
     }
 }
 
-function changeIcon(index, iconPath) {
-    inputsPassword[index].style.background = `url(${iconPath}) no-repeat`;
-    inputsPassword[index].style.backgroundPosition = 'right 21px top 50%';
+/**
+ * Change the icon of the password input
+ * @param {string} iconPath Icon path example: /img/image.svg
+ */
+function changePasswordInputIcon(iconPath) {
+    inputPassword.style.background = `url(${iconPath}) no-repeat`;
+    inputPassword.style.backgroundPosition = 'right 21px top 50%';
 }
 
-function checkInputPassword(index) {
-
-    if (inputsPassword[index].value) {
-        if (inputsPassword[index].type == 'password') {
-            changeIcon(index, '/img/visibility_off.png');
+/**
+ * Check every keypress on password input what type of input currently is to show the right button
+ */
+function checkInputPassword() {
+    if (inputPassword.value) {
+        if (inputPassword.type == 'password') {
+            changePasswordInputIcon('/img/visibility_off.png');
         } else if (inputPassword.type == 'text') {
-            changeIcon(index, '/img/visibility.png');
+            changePasswordInputIcon('/img/visibility.png');
         }
         showButtonPasswordVisible();
     } else {
-        changeIcon(index, '/img/lock.png');
+        changePasswordInputIcon('/img/lock.png');
         hideButtonPasswordVisible();
     }
 }
 
-function hidePassword(index) {
-    if (inputsPassword[index].type == 'password') {
-        inputsPassword[index].type = 'text';
-        changeIcon(index, '/img/visibility.png');
-    } else if (inputsPassword[index].type == 'text') {
-        inputsPassword[index].type = 'password';
-        changeIcon(index, '/img/visibility_off.png');
+/**
+ * Switch function to switch between hide and show password icon
+ */
+function hidePassword() {
+    if (inputPassword.type == 'password') {
+        inputPassword.type = 'text';
+        changePasswordInputIcon('/img/visibility.png');
+    } else if (inputPassword.type == 'text') {
+        inputPassword.type = 'password';
+        changePasswordInputIcon('/img/visibility_off.png');
     }
-    inputsPassword[index].focus();
+    inputPassword.focus();
 }
--- sign_up/sign_up.js
+++ sign_up/sign_up.js
@@ -1,14 +1,23 @@
-function checkPasswordMatch(form) {
-    console.log(true)
-    if (form['password'].value != form['confirm-password'].value) {
-        document.querySelector('div[class="validation-password"]').style.display = '';
-        form['confirm-password'].classList.add('input-danger');
+let dialogMessage = document.getElementById('dialog-message');
+let validationPassword = document.querySelector('div[class="validation-password"]');
+let buttonSignup = document.getElementById('button-sign-up');
+let allInputs = document.getElementsByClassName('input-field');
+let checkboxAccept = document.getElementById('accept');
+let signUpForm = document.getElementById('sign-up-form');
+
+/**
+ * Check if the password and password-repeat input match
+ * @returns {boolean} Return true if the passwords match and return false if not
+ */
+function checkPasswordMatch() {
+    if (signUpForm['password'].value != signUpForm['confirm-password'].value) {
+        validationPassword.style.display = '';
+        signUpForm['confirm-password'].classList.add('input-danger');
         return false;
     }
-    console.log(true)
-    document.getElementById('dialog-message').style.display = '';
+    dialogMessage.style.display = '';
     setTimeout(function () {
-        form.submit();
+        signUpForm.submit();
         users.push(
             {
                 'email': 'bla@join.com',
@@ -20,8 +29,10 @@ function checkPasswordMatch(form) {
     return false;
 }
 
+/**
+ * Signup the user and redirect them to the login page
+ */
 function signup() {
-    let buttonSignup = document.getElementById('button-sign-up');
     if (buttonSignup.hasAttribute('disabled')) {
         window.scrollTo(0, 0);
         document.body.style.overflow = 'hidden';
@@ -29,20 +40,18 @@ function signup() {
     }
 }
 
+/**
+ * Check if all inputs are filled out if not a alert message will be showed
+ */
 function checkIsInputFilled() {
-    let inputs = document.getElementsByClassName('input-field');
-    let checkboxAccept = document.getElementById('accept');
-    let buttonSignup = document.getElementById('button-sign-up');
-
     if (checkboxAccept.checked) {
         buttonSignup.removeAttribute('disabled');
     } else {
         buttonSignup.setAttribute('disabled', '');
 
     }
-
-    for (let i = 0; i < inputs.length; i++) {
-        let input = inputs[i];
+    for (let i = 0; i < allInputs.length; i++) {
+        let input = allInputs[i];
         if (!input.value) {
             buttonSignup.setAttribute('disabled', '');
             break
